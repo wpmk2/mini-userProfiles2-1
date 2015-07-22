924eae037b03b02961cae07404ccab68e96a514a
Wilson Parrish <wilsonparrishmk2@gmail.com>
2015-07-22 10:49:35 -0600

    done! promises are weird

--- controller.js
+++ controller.js
@@ -3,10 +3,11 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers().then(function(response) {
+      $scope.users = response.data.data;
+    });
   }
-
-
+  
   $scope.getUsers();
 
 });
